95dd820dbaf9efff4fa055f4aa6ee5fb47d9a7df
Chansong Jo <ddpghgg@gmail.com>
2019-11-25 11:48:24 +0900

    add NaiveDropoutMLP

--- convlab/agent/net/mlp.py
+++ convlab/agent/net/mlp.py
@@ -124,6 +124,121 @@ class MLPNet(Net, nn.Module):
             return self.model_tail(x)
 
 
+class NaiveDropoutMLPNet(Net, nn.Module):
+    '''
+    Class for generating arbitrary sized feedforward neural network
+    If more than 1 output tensors, will create a self.model_tails instead of making last layer part of self.model
+
+    e.g. net_spec
+    "net": {
+        "type": "MLPNet",
+        "shared": true,
+        "hid_layers": [32],
+        "hid_layers_activation": "relu",
+        "out_layer_activation": null,
+        "init_fn": "xavier_uniform_",
+        "clip_grad_val": 1.0,
+        "loss_spec": {
+          "name": "MSELoss"
+        },
+        "optim_spec": {
+          "name": "Adam",
+          "lr": 0.02
+        },
+        "lr_scheduler_spec": {
+            "name": "StepLR",
+            "step_size": 30,
+            "gamma": 0.1
+        },
+        "update_type": "replace",
+        "update_frequency": 1,
+        "polyak_coef": 0.9,
+        "gpu": true
+    }
+    '''
+
+    def __init__(self, net_spec, in_dim, out_dim):
+        '''
+        net_spec:
+        hid_layers: list containing dimensions of the hidden layers
+        hid_layers_activation: activation function for the hidden layers
+        out_layer_activation: activation function for the output layer, same shape as out_dim
+        init_fn: weight initialization function
+        clip_grad_val: clip gradient norm if value is not None
+        loss_spec: measure of error between model predictions and correct outputs
+        optim_spec: parameters for initializing the optimizer
+        lr_scheduler_spec: Pytorch optim.lr_scheduler
+        update_type: method to update network weights: 'replace' or 'polyak'
+        update_frequency: how many total timesteps per update
+        polyak_coef: ratio of polyak weight update
+        gpu: whether to train using a GPU. Note this will only work if a GPU is available, othewise setting gpu=True does nothing
+        '''
+        nn.Module.__init__(self)
+        super().__init__(net_spec, in_dim, out_dim)
+        # set default
+        util.set_attr(self, dict(
+            out_layer_activation=None,
+            init_fn=None,
+            clip_grad_val=None,
+            loss_spec={'name': 'MSELoss'},
+            optim_spec={'name': 'Adam'},
+            lr_scheduler_spec=None,
+            update_type='replace',
+            update_frequency=1,
+            polyak_coef=0.0,
+            gpu=False,
+            dropout_rate=0.25
+        ))
+        util.set_attr(self, self.net_spec, [
+            'shared',
+            'hid_layers',
+            'hid_layers_activation',
+            'out_layer_activation',
+            'init_fn',
+            'clip_grad_val',
+            'loss_spec',
+            'optim_spec',
+            'lr_scheduler_spec',
+            'update_type',
+            'update_frequency',
+            'polyak_coef',
+            'gpu',
+            'dropout_rate'
+        ])
+
+        dims = [self.in_dim] + self.hid_layers
+        self.model = net_util.build_dropout_model(dims, self.hid_layers_activation, self.dropout_rate)
+        # add last layer with no activation
+        # tails. avoid list for single-tail for compute speed
+        if ps.is_integer(self.out_dim):
+            self.model_tail = net_util.build_fc_model([dims[-1], self.out_dim], self.out_layer_activation)
+        else:
+            if not ps.is_list(self.out_layer_activation):
+                self.out_layer_activation = [self.out_layer_activation] * len(out_dim)
+            assert len(self.out_layer_activation) == len(self.out_dim)
+            tails = []
+            for out_d, out_activ in zip(self.out_dim, self.out_layer_activation):
+                tail = net_util.build_fc_model([dims[-1], out_d], out_activ)
+                tails.append(tail)
+            self.model_tails = nn.ModuleList(tails)
+
+        net_util.init_layers(self, self.init_fn)
+        self.loss_fn = net_util.get_loss_fn(self, self.loss_spec)
+        self.to(self.device)
+        self.train()
+
+    def forward(self, x):
+        '''The feedforward step'''
+        x = self.model(x)
+        if hasattr(self, 'model_tails'):
+            outs = []
+            for model_tail in self.model_tails:
+                outs.append(model_tail(x))
+            return outs
+        else:
+            return self.model_tail(x)
+
+
 class HydraMLPNet(Net, nn.Module):
     '''
     Class for generating arbitrary sized feedforward neural network with multiple state and action heads, and a single shared body.
